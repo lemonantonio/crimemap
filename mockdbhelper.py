5204647c2ee81650bddd4d80d240e81fc136e0d5
lemonantonio <lemonantonio@163.com>
2018-07-22 08:34:26 +0800

    add new crimes funtionality

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -18,3 +18,14 @@ class MockDBHelper(object):
 
     def clear_all(self):
         pass
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        pass
+
+    def get_all_crimes(self):
+        return [{'latitude': -33.301304,
+                  'longitide': 26.523355,
+                  'date': '2000-01-01',
+                  'category': 'mugging',
+                  'description': 'mock description'}]
+
